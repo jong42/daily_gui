085629f8a4d75dded8bdda525b85114b66c80c9c
Jonas <jonasguetter@gmx.de>
2023-05-05 13:03:24 +0200

    Start integrsting pyplot into GUI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 #import requests
-
-
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 ### handle api_call ###
 #api_address = "api.openweathermap.org/data/2.5/forecast?q=Jena,ger&appid=ca3c615c20062c4fed7b25374cb16a77"
@@ -28,12 +28,32 @@ for entry in api_result['list']:
     temps.append(temp)
     weather.append(weather_status)
     prec_probs.append(prec_prob)
+### pyplot integration to PySImpleGUI boilerplate
+
+# VARS CONSTS:
+_VARS = {'window': False}
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+
+###Construct GUI ###
+
+layout = [[sg.Text("This is a line of text")], [sg.Text(temps[0])], [sg.Canvas(key='figCanvas')]]
+_VARS['window'] = sg.Window("daily_gui", layout)
+
+
 
 ### Create matplotlib plot ###
 
-###Include plot into gui ###
+fig = plt.figure()
+plt.plot(timestamps, temps)
+# Instead of plt.show
+draw_figure(_VARS['window']['figCanvas'].TKCanvas, fig)
 
-layout = [[sg.Text("This is a line of text")], [sg.Text("This is another line of text")]]
-window = sg.Window("daily_gui", layout)
-
-window.read()
+### show GUI
+_VARS['window'].read()
+_VARS['window'].close()
