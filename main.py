2d070bd2586813cac6410e9f8a3c851e2aab0ddc
Jonas <jonasguetter@gmx.de>
2023-05-12 15:08:33 +0200

    Add download, storing and loading of weatherdata

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-#import requests
+import os
+import json
+import requests
+import datetime
 from backend import extract_vals_from_dict
 from frontend import init_gui, add_figs_to_gui
 
-# Download data
-#api_address = "api.openweathermap.org/data/2.5/forecast?q=Jena,ger&appid=ca3c615c20062c4fed7b25374cb16a77"
-#api_result = requests.get(api_address)
+location = "Jena"
 
-# temporary workaround
-import json
-f = open("/home/jonas/Desktop/forecast.json")
+# Check if data has already been downloaded today
+now = datetime.datetime.now()
+weatherfiles = os.listdir("/home/jonas/Desktop/daily_gui/data/weather_data/")
+date_now = now.strftime("%Y_%m_%d")
+already_downloaded = any([date_now in filename for filename in weatherfiles])
+
+if not already_downloaded:
+    # Download and store data
+    api_address = "api.openweathermap.org/data/2.5/forecast?q=" + location + ",units=metric,ger&appid=ca3c615c20062c4fed7b25374cb16a77"
+    api_result = requests.get(api_address)
+    filepath = now.strftime("%Y_%m_%d_%H_%M_") + location + ".json"
+    with open(filepath,"w") as fp:
+        json.dump(api_result, fp)
+
+# Load data
+f = open(filepath)
 api_result = json.load(f)
 
 # Extract values
