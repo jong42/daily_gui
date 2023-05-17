04431ead932c0188da470ac07cfe166a2c99bbf6
Jonas <jonasguetter@gmx.de>
2023-05-17 16:18:11 +0200

    Add point labels for min and amx temperature

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import json
 import datetime
+import numpy as np
+import PySimpleGUI as sg
 from backend import check_filenames, extract_vals_from_dict, download_weather_data
-from frontend import init_gui, add_figs_to_gui
+from frontend import init_layout, init_gui, add_figs_to_gui
 
 location = "Jena"
 weather_data_path = "/home/jonas/Desktop/daily_gui/data/weather_data/"
@@ -24,9 +26,35 @@ api_result = json.load(f)
 # Extract values
 timestamps, temps, weather, prec_probs = extract_vals_from_dict(api_result)
 
+# Get min and max temperature per day
+minmax_timestamps = []
+minmax_temps = []
+unique_dates = np.unique([i[0:10] for i in timestamps])
+for date in unique_dates:
+    # Get all data points corresponding to a given date
+    sub_timestamp_indices = np.asarray([date in i for i in timestamps]).nonzero()[0]
+    sub_timestamps = [timestamps[i] for i in sub_timestamp_indices]
+    sub_temps = [temps[i] for i in sub_timestamp_indices]
+    # Get exact timestamp and temperature for max and min temperature on that date
+    max_temp = np.max(sub_temps)
+    min_temp = np.min(sub_temps)
+    max_temp_index = sub_temps.index(max_temp)
+    min_temp_index = sub_temps.index(min_temp)
+    max_temp_timestamp = sub_timestamps[max_temp_index]
+    min_temp_timestamp = sub_timestamps[min_temp_index]
+    # Add results to corresponding lists
+    minmax_timestamps.append(max_temp_timestamp)
+    minmax_timestamps.append(min_temp_timestamp)
+    minmax_temps.append(max_temp)
+    minmax_temps.append(min_temp)
+
 # Construct GUI
-gui = init_gui()
-add_figs_to_gui(gui, timestamps, temps, prec_probs)
+layout = init_layout()
+layout.append([sg.Text('This is text')])
+gui = init_gui(layout)
+
+#gui = init_gui()
+add_figs_to_gui(gui, timestamps, temps, prec_probs, minmax_timestamps, minmax_temps)
 
 # show GUI
 gui.read()
