240737e5e45be3b95dfddf3f246b6cd0ad6ede91
Jonas <jonasguetter@gmx.de>
2023-05-17 16:18:31 +0200

    Add point labels for min and amx temperature

--- frontend.py
+++ frontend.py
@@ -13,21 +13,38 @@ def draw_figure(canvas, figure):
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
 
-def init_gui():
+def init_layout():
+    """
+    Initialize a layout with a blank canvas for a matplotlib plot. For adding contents to the canvas, init_gui() has
+    to be called, then figures can be added with add_fig_to_gui()
+    :return: layout. a PySimpleGUI layout
+    """
+    layout = [[sg.Canvas(key='figCanvas')]]
+    return layout
+
+def init_gui(layout):
     """
     Construct a GUI with PySimpleGUI. Figures have to be added afterwards, see add_fig_to_gui()
+
+    layout: PySimpleGUI layout
     """
-    layout = [[sg.Text("This is a line of text")], [sg.Canvas(key='figCanvas')]]
+    #layout = [[sg.Text("This is a line of text")], [sg.Canvas(key='figCanvas')]]
     window = sg.Window("daily_gui", layout, finalize=True)
     return window
 
-def add_figs_to_gui(gui, timestamps:List, temps:List, prec_probs:List):
+def add_figs_to_gui(gui, timestamps:List, temps:List, prec_probs:List, minmax_timestamps:List, minmax_temps:List):
     """
     Add pyplot figures to an existing GUI created with PySimpleGUI
     """
     # TODO Combine temp and prec_prob into one plot with different axis
+    # TODO: Write proper docstrings for all the functions here
     fig, axs = plt.subplots(1, 2, figsize=(16, 9))
     axs[0].plot(timestamps, temps, color='red')
+    axs[0].scatter(timestamps, temps)
+    axs[0].scatter(minmax_timestamps, minmax_temps)
+    # Label points
+    for (x, y) in zip(minmax_timestamps, minmax_temps):
+        axs[0].text(x, y, str(y)[0:4])
     axs[1].plot(timestamps, prec_probs)
     axs[0].set_title('Temperature')
     axs[1].set_title('Precipitation Probability')
