b50764315c472af75604a446c5525d2607c82946
Jonas <jonasguetter@gmx.de>
2023-05-18 15:59:53 +0200

    Test commit for pre-commit hook

--- main.py
+++ main.py
@@ -54,9 +54,7 @@ layout.append([sg.Text("This is text")])
 gui = init_gui(layout)
 
 # gui = init_gui()
-add_figs_to_gui(
-    gui, timestamps, temps, prec_probs, minmax_timestamps, minmax_temps
-)  # randomcomment
+add_figs_to_gui(gui, timestamps, temps, prec_probs, minmax_timestamps, minmax_temps)
 
 # show GUI
 gui.read()
