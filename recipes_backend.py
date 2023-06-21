67d280121b7b51e742f2941cc7bcea0859138cdf
Jonas <jonasguetter@gmx.de>
2023-06-21 14:49:21 +0200

    Prototype loading recipe from json file

--- recipes_backend.py
+++ recipes_backend.py
@@ -1,4 +1,5 @@
 from typing import List
+import json
 
 
 class Recipe:
@@ -9,7 +10,16 @@ class Recipe:
 
 
 if __name__ == "__main__":
-    rec_name = "Reis mit Fisch"
-    ingredients = ["Lachs", "Reis", "Öl", "saure Sahne", "Kräuter zum Würzen", "Gemüse"]
-    preparation = ""
-    recipe1 = Recipe(rec_name, ingredients, preparation)
+    recipes_path = "/home/jonas/Desktop/daily_gui/data/recipes.json"
+    with open(recipes_path, "r") as recipes_data:
+        recipes_json = json.load(recipes_data)
+    print(recipes_json)
+    for entry in recipes_json:
+        name = recipes_json[entry]['name']
+        ingredients = recipes_json[entry]['ingredients']
+        preparation = recipes_json[entry]['preparation']
+        recipe = Recipe(name, ingredients, preparation)
+
+    print(recipe.name)
+    print(recipe.ingredients)
+    print(recipe.preparation)
